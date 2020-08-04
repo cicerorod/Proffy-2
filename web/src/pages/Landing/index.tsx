3ddd74c8764ff0cfd56e916c9d504df49d44c923
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-08-03 21:55:39 -0300

    feat(web): criação da landing page

---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import logoImg from '../../assets/logo.svg';
+import landingImg from '../../assets/landing.svg';
+import studyIcon from '../../assets/icons/study.svg';
+import giveClassesIcon from '../../assets/icons/give-classes.svg';
+import purpleHeart from '../../assets/icons/purple-heart.svg';
+
+import './styles.css';
+import { Link } from 'react-router-dom';
+
+export default function Landing() {
+  return (
+    <main id="page-landing">
+      <section id="page-landing-content" className="container">
+        <div className="logo-container">
+          <img src={ logoImg } alt="Proffy" />
+          <h2>Sua plataforma de estudos online.</h2>
+        </div>
+
+        <img
+          src={ landingImg }
+          alt="Plataforma de estudos"
+          className="hero-image"
+        />
+
+        <div className="buttons-container">
+          <Link to="/study" className="study">
+            <img src={ studyIcon } alt="Estudar" />
+            Estudar
+          </Link>
+
+          <Link to="/give-classes" className="give-classes">
+            <img src={ giveClassesIcon } alt="Dar aulas" />
+            Dar aulas
+          </Link>
+        </div>
+
+        <span className="total-connections">
+          Total de 200 conexões já realizadas
+          <img src={ purpleHeart } alt="Coração roxo" />
+        </span>
+      </section>
+    </main>
+  );
+}

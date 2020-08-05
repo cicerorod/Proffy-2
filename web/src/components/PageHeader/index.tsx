ed48f4707799ae1123c4649ab32c032b2554a46c
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-08-05 19:10:44 -0300

    feat(web): criação da estrutura da página TeacherForm

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -7,6 +7,7 @@ import './styles.css';
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
 };
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
@@ -22,10 +23,10 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
 
       <div className="header-content">
         <strong> { props.title } </strong>
-        {
-          props.children
-        }
-      </div>      
+        { props.description && <p> { props.description }</p> }
+
+        { props.children }
+      </div>
     </header>
   );
 };
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,42 +1,63 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
-
-
-
-import './style.css';
 import TeacherItem from '../../components/TeacherItem';
+import './style.css';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 export default function TeacherList(): JSX.Element {
   return (
     <main id="page-teacher-list" className="container">
-
       <PageHeader title="Estes são os professores disponíveis.">
+
         <form id="search-teachers">
+          <Select
+            name="suject"
+            label="Matéria"
+            options={ [
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Ed.Física', label: 'Ed.Física' },
+              { value: 'Espanhol', label: 'Espanhol' },
+              { value: 'Filosofia', label: 'Filosofia' },
+              { value: 'Física', label: 'Física' },
+              { value: 'Geografia', label: 'Geografia' },
+              { value: 'História', label: 'História' },
+              { value: 'Inglês', label: 'Inglês' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Português', label: 'Português' },
+              { value: 'Química', label: 'Química' },
+              { value: 'Sociologia', label: 'Sociologia' },
+            ] }
+          />
 
-          <div className="input-block">
-            <label htmlFor="subject">Matérias</label>
-            <input type="text" id="subject" />
-          </div>
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            options={ [
+              { value: '0', label: 'Domingo' },
+              { value: '1', label: 'Segunda-feira' },
+              { value: '2', label: 'Terça-feira' },
+              { value: '3', label: 'Quarta-feira' },
+              { value: '4', label: 'Quinta-feira' },
+              { value: '5', label: 'Sexta-feira' },
+              { value: '6', label: 'Sábado' },
+            ] }
+          />
 
-          <div className="input-block">
-            <label htmlFor="week-day">Dia da semana</label>
-            <input type="text" id="week-day" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Horário</label>
-            <input type="text" id="time" />
-          </div>
-
+          <Input type="time" name="time" label="Hora" />
         </form>
+
       </PageHeader>
 
-      <TeacherItem />
-      <TeacherItem />
-      <TeacherItem />
-      <TeacherItem />
-      <TeacherItem />
-      
+      <section id="list-cards-container">
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </section>
+
 
     </main>
   );
